04af3ca59cf6ab85f011a7cec18d917c68fa9c6d
Koltyn Palmer <palmer@ring.com>
2018-07-24 00:59:50 -0600

    still goin w the serach filters

--- v2/public/js/searchFilter.js
+++ v2/public/js/searchFilter.js
@@ -1,124 +1,4 @@
-// // // Logic to filter using .search-key filters:
-// function searchAll() {
-//     let trs, tds;
-//     const vlu = [];
-
-//     // Find all search filters:
-//     const searchKey$ = Array.from(document.getElementsByTagName('.search-key'));
-//     console.log(searchKey$``);
-
-//     // Get each value entered, upon keyUp:
-//     for (key in searchKey$) {
-//         var contents = $('#tableFilter'[key]);
-//         vlu.push(document.getElementById(searchKey$[key]).value); // does not return the values
-//         const searchVal = $(this).val();
-//         // const searchVal2 = document.getElementById(searchKeys[d]).value;
-//         console.log(contents);
-//         console.log(searchVal);
-//         console.log(searchVal2);
-//         // $(d).text(searchVal);
-//         console.log($(searchKeys).innerHTML);
-//         console.log($(searchKeys).value());
-//         // vlu.push((searchKey[d]).value());
-//         let vluu = $(searchKeys).val();
-//         console.log(vluu);
-//     };
-
-//     // // Create an array which records each table row.
-//     trs = Array.from($('tr td'));
-//     // console.log(trs);
-//     vlu.push(document.getElementById(input[d]).value);
-//     // // For each table row, let tr = row.
-//     trs.forEach(function (tr, i) {
-
-//         let filters = [];
-//         vlu.forEach((filter, j) =>
-//             filters.push(filter.toUpperCase()));
-
-//         // const testhere$ = new Array($('tr td')); // prints classes!
-//         const testhere$ = Array.from(document.getElementsByTagName('tr td'));
-//         testhere$.forEach(function(i) {
-
-//             if (testhere$) {
-//                 for (index in testhere$) {
-//                     if (testhere$.innerHTML.toUpperCase.indexOf(index[i]) > -1) {
-//                         tr.style.display = "";
-//                     } else {
-//                         tr.style.display = "none"
-//                     }
-//                 }
-//             };
-//         });
-//     });
-// };
-
-
-// // // // // // // // // // // // // // // // // // //
-// // // // // PREVIOUSLY WORKING VERSION // // // // // 
-// // // // // // // // // // // // // // // // // // //
-
-
-// // <!-- 
-//     function searchAll() {
-//         let table, trs, tds;
-//         table = document.getElementById('tableBody');
-//         trs = Array.from(table.getElementsByTagName('tr'));
-
-//         const input = 
-//         ['tableInput1',
-//         'tableInput2',
-//         'tableInput3',
-//         'tableInput4',
-//         'tableInput5',
-//         'tableInput6',
-//         'tableInput7',
-//         'tableInput8',
-//         'tableInput9',
-//         'tableInput10',
-//         'tableInput11',
-//         'tableInput12',
-//         'tableInput13',
-//         'tableInput14',
-//         'tableInput15'];
-
-//         const vlu = [];
-//         for (d in input) {
-//             vlu.push($('input'[d]).val);
-//         }
-//         console.log(vlu);
-
-//         trs.forEach(function (tr, i) {
-
-//             let filters = [];
-//             vlu.forEach((filter, j) =>
-//                 filters.push(filter.toUpperCase()));
-//             // console.log(filters)
-
-//             const testhere = tr.getElementsByTagName("td");
-
-//             if (testhere[2] && testhere[3] && testhere[4] && testhere[5] && testhere[6] && testhere[7] && testhere[8] && testhere[9] && testhere[10] && testhere[11] && testhere[12] && testhere[13] && testhere[14] && testhere[15] && testhere[16]) {
-//                 if (testhere[2].innerHTML.toUpperCase().indexOf(filters[0]) > -1 &&
-//                     testhere[3].innerHTML.toUpperCase().indexOf(filters[1]) > -1 &&
-//                     testhere[4].innerHTML.toUpperCase().indexOf(filters[2]) > -1 &&
-//                     testhere[5].innerHTML.toUpperCase().indexOf(filters[3]) > -1 &&
-//                     testhere[6].innerHTML.toUpperCase().indexOf(filters[4]) > -1 &&
-//                     testhere[7].innerHTML.toUpperCase().indexOf(filters[5]) > -1 &&
-//                     testhere[8].innerHTML.toUpperCase().indexOf(filters[6]) > -1 &&
-//                     testhere[9].innerHTML.toUpperCase().indexOf(filters[7]) > -1 &&
-//                     testhere[10].innerHTML.toUpperCase().indexOf(filters[8]) > -1 &&
-//                     testhere[11].innerHTML.toUpperCase().indexOf(filters[9]) > -1 &&
-//                     testhere[12].innerHTML.toUpperCase().indexOf(filters[10]) > -1 &&
-//                     testhere[13].innerHTML.toUpperCase().indexOf(filters[11]) > -1 &&
-//                     testhere[14].innerHTML.toUpperCase().indexOf(filters[12]) > -1 &&
-//                     testhere[15].innerHTML.toUpperCase().indexOf(filters[13]) > -1 &&
-//                     testhere[16].innerHTML.toUpperCase().indexOf(filters[14]) > -1) {
-//                     tr.style.display = "";
-//                 } else {
-//                     tr.style.display = "none"
-//                 }
-//             }
-//         });
-//     } 
+// Logic to filter using .search-key filters:
 
 
 function searchAll() {
@@ -144,33 +24,37 @@ function searchAll() {
         vlu.forEach((filter, j) =>
             filters.push(filter.toUpperCase()));
         // console.log(filters);
-        const testhere = tr.getElementsByTagName("td");
-        testhere$ = Array.from(tr.getElementsByTagName("td"));
+        const tablecell = tr.getElementsByTagName("td");
+        tablecell$ = Array.from(tr.getElementsByTagName("td"));
+        filters$ = Array.from(filters);
+        // console.log(tablecell$);
+        // console.log(filters$);
+        // console.log(filters);
 
-        // console.log(testhere$);
-
-        if (testhere$) {
-            if (testhere[0].innerHTML.toUpperCase().indexOf(filters[0]) > -1 &&
-                testhere[1].innerHTML.toUpperCase().indexOf(filters[1]) > -1 &&
-                testhere[2].innerHTML.toUpperCase().indexOf(filters[2]) > -1 &&
-                testhere[3].innerHTML.toUpperCase().indexOf(filters[3]) > -1 &&
-                testhere[4].innerHTML.toUpperCase().indexOf(filters[4]) > -1 &&
-                testhere[5].innerHTML.toUpperCase().indexOf(filters[5]) > -1 &&
-                testhere[6].innerHTML.toUpperCase().indexOf(filters[6]) > -1 &&
-                testhere[7].innerHTML.toUpperCase().indexOf(filters[7]) > -1 &&
-                testhere[8].innerHTML.toUpperCase().indexOf(filters[8]) > -1 &&
-                testhere[9].innerHTML.toUpperCase().indexOf(filters[9]) > -1 &&
-                testhere[10].innerHTML.toUpperCase().indexOf(filters[10]) > -1 &&
-                testhere[11].innerHTML.toUpperCase().indexOf(filters[11]) > -1 &&
-                testhere[12].innerHTML.toUpperCase().indexOf(filters[12]) > -1 &&
-                testhere[13].innerHTML.toUpperCase().indexOf(filters[13]) > -1 &&
-                testhere[14].innerHTML.toUpperCase().indexOf(filters[14]) > -1) 
+        if (tablecell$) {
+            for (index in tablecell$) {
+            if (tablecell[0].innerHTML.toUpperCase().indexOf(filters[0]) > -1 &&
+                tablecell[1].innerHTML.toUpperCase().indexOf(filters[1]) > -1 &&
+                tablecell[2].innerHTML.toUpperCase().indexOf(filters[2]) > -1 &&
+                tablecell[3].innerHTML.toUpperCase().indexOf(filters[3]) > -1 &&
+                tablecell[4].innerHTML.toUpperCase().indexOf(filters[4]) > -1 &&
+                tablecell[5].innerHTML.toUpperCase().indexOf(filters[5]) > -1 &&
+                tablecell[6].innerHTML.toUpperCase().indexOf(filters[6]) > -1 &&
+                tablecell[7].innerHTML.toUpperCase().indexOf(filters[7]) > -1 &&
+                tablecell[8].innerHTML.toUpperCase().indexOf(filters[8]) > -1 &&
+                tablecell[9].innerHTML.toUpperCase().indexOf(filters[9]) > -1 &&
+                tablecell[10].innerHTML.toUpperCase().indexOf(filters[10]) > -1 &&
+                tablecell[11].innerHTML.toUpperCase().indexOf(filters[11]) > -1 &&
+                tablecell[12].innerHTML.toUpperCase().indexOf(filters[12]) > -1 &&
+                tablecell[13].innerHTML.toUpperCase().indexOf(filters[13]) > -1 &&
+                tablecell[14].innerHTML.toUpperCase().indexOf(filters[14]) > -1) 
                 {
                 tr.style.display = "";
             } else {
                 tr.style.display = "none"
             }
-        }
+        };
+        };
     });
 };
 
