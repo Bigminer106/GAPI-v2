2907645d863c22404afca8ae041c2f96447b30a4
Koltyn Palmer <palmer@ring.com>
2018-07-24 14:42:40 -0600

    server change

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 
-var port = process.env.PORT || 8000;
+var serverPORT = 8000;
+var port = process.env.PORT || serverPORT;
 var app = express();
 
-// var PORT = 8000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
